87988fb4a1a3f979c933fdff54196cb739aad24d
Hector Rubi <hector.rubi.garcia@outlook.com>
2020-11-10 14:04:20 -0600

    Implement projects feature and improve certificates feature
    Imports elements were updated to add utils folder elements.
    Projects and certificates features render information in the
    constructor.

--- js/index.js
+++ js/index.js
@@ -1,10 +1,12 @@
-import HamburgerMenu from './HamburguerMenu.js';
-import Responsive from './plugins/Responsive.js';
-import Certificates from './Certificates.js'
+import HamburgerMenu from './utils/HamburguerMenu.js'
+import Responsive from './plugins/Responsive.js'
+import Certificates from './utils/Certificates.js'
 import dataCertificates from './data/dataCertificates.js'
+import Projects from './utils/Projects.js'
+import dataProjects from './data/dataProjects.js';
 
-const menu = new HamburgerMenu();
-const resp = new Responsive(menu);
+const menu = new HamburgerMenu()
+const resp = new Responsive(menu)
 
 const cert = new Certificates(dataCertificates)
-cert.renderCode();
+const proj = new Projects(dataProjects)
