29e60c75cee838513d0ba974346d769ec855e261
Hector Rubi Linuxero <hector.rubi.garcia@outlook.com>
2023-02-21 16:57:01 -0600

    Change design of hero element

--- src/js/index.js
+++ src/js/index.js
@@ -1,11 +1,9 @@
 import HamburgerMenu from './utils/HamburguerMenu.js'
-import Responsive from './plugins/Responsive.js'
 import Certificates from './utils/Certificates.js'
 import dataCertificates from './data/dataCertificates.js'
 import Projects from './utils/Projects.js'
 import dataProjects from './data/dataProjects.js';
 
 const menu = new HamburgerMenu()
-
 const cert = new Certificates(dataCertificates)
 const proj = new Projects(dataProjects)
