204c2699fdb151feb4eae50f171aa4a8b62e504c
Hector Rubi Linuxero <hector.rubi.garcia@outlook.com>
2023-03-02 20:15:19 -0600

    Remove unused dependencies

--- src/js/index.js
+++ src/js/index.js
@@ -1,8 +1,3 @@
 import HamburgerMenu from './utils/HamburguerMenu.js'
-import Certificates from './utils/Certificates.js'
-import dataCertificates from './data/dataCertificates.js'
-import Projects from './utils/Projects.js'
-import dataProjects from './data/dataProjects.js';
 
 const menu = new HamburgerMenu()
-const cert = new Certificates(dataCertificates)
