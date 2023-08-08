fd0992548435af432903d20bade97fe71941fda9
Hector Rubi <hector.rubi.garcia@outlook.com>
2023-08-07 23:39:03 -0600

    Add font awesome

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,12 @@ import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 
+import { config, library } from '@fortawesome/fontawesome-svg-core'
+import { fas } from '@fortawesome/free-solid-svg-icons'
+import '@fortawesome/fontawesome-svg-core/styles.css'
+config.autoAddCss = false
+library.add(fas)
+
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
