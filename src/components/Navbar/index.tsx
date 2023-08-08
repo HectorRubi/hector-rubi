2d4718822711fc215c1137506d0a2c49d48408ce
Hector Rubi <hector.rubi.garcia@outlook.com>
2023-08-08 16:32:33 -0600

    Add email icon and create a tooltip for it

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,9 +1,12 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons'
 import {
   faLinkedin,
   faGithub,
   IconDefinition,
 } from '@fortawesome/free-brands-svg-icons'
+
+import { Tooltip } from './../Tooltip'
 
 import { CONTAINER } from '../constants'
 
@@ -28,31 +31,40 @@ const links: Link[] = [
 
 export const Navbar = () => {
   return (
-    <nav className="bg-slate-400 dark:bg-slate-900">
-      <div className={`${CONTAINER} flex justify-between items-center py-16`}>
+    <nav className="bg-primary-400 dark:bg-primary-900">
+      <div className={`${CONTAINER} flex items-center justify-between py-16`}>
         <h3 className="text-2xl">
           <span>Hector </span>
-          <span className="text-zinc-600 dark:text-zinc-500">Rubí</span>
+          <span className="text-secondary-600 dark:text-secondary-500">
+            Rubí
+          </span>
         </h3>
 
-        <ul className="flex gap-5">
+        <ul className="flex items-center gap-5">
           {links.map((link, linkKey) => (
             <li key={linkKey}>
               <a
                 href={link.url}
                 target="_blank"
-                className="group flex items-center text-sm hover:text-amber-400 dark:hover:text-amber-500 transition-all"
+                className="group flex items-center"
               >
                 <FontAwesomeIcon
                   icon={link.icon}
-                  className="inline-block text-lg mr-2"
+                  className={`mr-2 text-lg transition-all group-hover:text-accent-400`}
                 />
-                <span className="inline-block text-zinc-600 dark:text-zinc-500 group-hover:text-amber-400 dark:group-hover:text-amber-500 transition-all">
+                <span
+                  className={`text-sm text-secondary-600 transition-all group-hover:text-accent-400 dark:text-secondary-500`}
+                >
                   {link.name}
                 </span>
               </a>
             </li>
           ))}
+
+          <li className="group relative flex cursor-pointer items-center justify-center rounded-full bg-primary-300 px-3 py-3 transition-all hover:bg-primary-800 hover:text-accent-400 dark:bg-primary-800 dark:hover:bg-primary-600">
+            <FontAwesomeIcon icon={faEnvelope} />
+            <Tooltip text="hector.rubi.garcia@outlook.com" position="right" />
+          </li>
         </ul>
       </div>
     </nav>
