5a2bc11ed5d6d82b2167b661f9f7505d0adb2aff
Hector Rubi <hector.rubi.garcia@outlook.com>
2023-08-10 11:35:57 -0600

    Isolate projects data

--- src/components/LatestWorks/index.tsx
+++ src/components/LatestWorks/index.tsx
@@ -1,36 +1,7 @@
 import Image from 'next/image'
+
 import { CONTAINER } from '../constants'
-
-type LatestWorks = {
-  name: string
-  stack: string[]
-  image: string
-  link: string
-}
-
-const latestWorks: LatestWorks[] = [
-  {
-    name: 'Tic Tac Toe Game',
-    stack: ['HTML', 'CSS', 'JavaScript', 'POO'],
-    image:
-      'https://github.com/HectorRubi/hector-rubi/assets/68661875/2720d4a1-9a2d-462b-a317-0b3ccd97b07a',
-    link: 'https://hectorrubi.github.io/tic-tac-toe-javascript/',
-  },
-  {
-    name: 'Skolar',
-    stack: ['HTML', 'CSS', 'JavaScript', 'PHP', 'MySQL'],
-    image:
-      'https://github.com/HectorRubi/hector-rubi/assets/68661875/bccea143-b1c7-4477-871c-f72d09615581',
-    link: 'https://skolar.mx',
-  },
-  {
-    name: 'Cremapet',
-    stack: ['HTML', 'CSS', 'Bootstrap'],
-    image:
-      'https://github.com/HectorRubi/hector-rubi/assets/68661875/19d19c7b-ed9c-41e2-a643-7af70f910f64',
-    link: 'https://www.cremapet.com/',
-  },
-]
+import { projects } from '../../data/projects'
 
 export const LatestWorks = () => {
   return (
@@ -45,24 +16,24 @@ export const LatestWorks = () => {
           </p>
         </div>
 
-        {latestWorks.map((latestWork, latestWorkKey) => (
+        {projects.map((project, projectIndex) => (
           <article
-            key={latestWorkKey}
+            key={projectIndex}
             className="group row-span-2 flex flex-col justify-between overflow-hidden rounded-lg bg-primary-500 transition-all hover:-translate-y-1 hover:bg-primary-300 hover:shadow-2xl hover:shadow-black/80 dark:bg-primary-800 dark:hover:bg-primary-700"
           >
             <div className="flex flex-col gap-5 px-5 py-10 sm:px-10 md:px-14 md:py-16 xl:flex-row xl:items-center xl:justify-between">
               <h3 className="text-xl md:text-2xl lg:text-3xl">
                 <a
-                  href={latestWork.link}
+                  href={project.link}
                   target="_blank"
                   className="transition-all hover:text-accent-600 hover:underline hover:underline-offset-8 dark:hover:text-accent-500"
                 >
-                  {latestWork.name}
+                  {project.name}
                 </a>
               </h3>
 
               <p className="flex flex-wrap gap-3">
-                {latestWork.stack.map((tech, techKey) => (
+                {project.stack.map((tech, techKey) => (
                   <span
                     key={techKey}
                     className="rounded bg-accent-600 px-2 py-1 text-xs text-white shadow"
@@ -76,8 +47,8 @@ export const LatestWorks = () => {
             <figure className="relative after:absolute after:bottom-0 after:left-0 after:right-0 after:top-0 after:bg-black after:opacity-30 after:transition-all group-hover:after:opacity-0">
               <Image
                 className="object-cover lg:rounded"
-                src={latestWork.image}
-                alt={latestWork.name}
+                src={project.image}
+                alt={project.name}
                 width={1920}
                 height={1080}
               />
---
+++ src/data/projects.ts
@@ -0,0 +1,30 @@
+export type Project = {
+  name: string
+  stack: string[]
+  image: string
+  link: string
+}
+
+export const projects: Project[] = [
+  {
+    name: 'Tic Tac Toe Game',
+    stack: ['HTML', 'CSS', 'JavaScript', 'POO'],
+    image:
+      'https://github.com/HectorRubi/hector-rubi/assets/68661875/2720d4a1-9a2d-462b-a317-0b3ccd97b07a',
+    link: 'https://hectorrubi.github.io/tic-tac-toe-javascript/',
+  },
+  {
+    name: 'Skolar',
+    stack: ['HTML', 'CSS', 'JavaScript', 'PHP', 'MySQL'],
+    image:
+      'https://github.com/HectorRubi/hector-rubi/assets/68661875/bccea143-b1c7-4477-871c-f72d09615581',
+    link: 'https://skolar.mx',
+  },
+  {
+    name: 'Cremapet',
+    stack: ['HTML', 'CSS', 'Bootstrap'],
+    image:
+      'https://github.com/HectorRubi/hector-rubi/assets/68661875/19d19c7b-ed9c-41e2-a643-7af70f910f64',
+    link: 'https://www.cremapet.com/',
+  },
+]
