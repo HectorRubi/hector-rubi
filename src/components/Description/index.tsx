ec2158c944c3f6a17c775c3cce9daaaebb73a4aa
Hector Rubi <hector.rubi.garcia@outlook.com>
2023-08-09 19:55:06 -0600

    Fix responsive of description component

--- src/components/Description/index.tsx
+++ src/components/Description/index.tsx
@@ -11,7 +11,7 @@ import { CONTAINER } from '../constants'
 type Skills = {
   name: string
   description: string
-  stat: string
+  stat: number
   icon: IconDefinition
 }
 
@@ -19,19 +19,19 @@ const skills: Skills[] = [
   {
     name: 'Front-End',
     description: 'Create web pages with coding super smooth.',
-    stat: '10 Projects',
+    stat: 10,
     icon: faCode,
   },
   {
     name: 'Back-End',
     description: 'I develop back-end with smart thinking.',
-    stat: '5 Projects',
+    stat: 5,
     icon: faTerminal,
   },
   {
     name: 'Database',
     description: 'I know store data on easy way.',
-    stat: '1 Projects',
+    stat: 1,
     icon: faDatabase,
   },
 ]
@@ -39,35 +39,37 @@ const skills: Skills[] = [
 export const Description = () => {
   return (
     <section className="bg-primary-400 dark:bg-primary-900">
-      <div className={`${CONTAINER} flex flex-col py-36 md:flex-row`}>
+      <div className={`${CONTAINER} flex flex-col-reverse py-36 md:flex-row`}>
         <div className="md:w-1/2">
           {skills.map((skill, index) => (
             <div
               key={index}
               className="mb-2 rounded-lg bg-primary-300 p-10 shadow-none transition-all hover:z-10 hover:-translate-y-1 hover:shadow-2xl hover:shadow-black/50 dark:bg-primary-800 dark:shadow-white"
             >
-              <div className="flex items-center justify-between text-xl text-accent-600 drop-shadow-sm dark:text-accent-400">
+              <div className="flex items-center justify-between text-xl text-accent-600 drop-shadow-sm dark:text-accent-400 lg:text-2xl">
                 <h3 className="relative mb-2">{skill.name}</h3>
                 <span>
                   <FontAwesomeIcon icon={skill.icon} />
                 </span>
               </div>
-              <p className="mb-4 text-sm text-zinc-600 dark:text-zinc-400">
+              <p className="mb-8 text-base text-zinc-600 dark:text-zinc-400">
                 {skill.description}
               </p>
-              <p className="text-sm">{skill.stat}</p>
+              <p className="text-sm">{`${skill.stat} Project${
+                skill.stat > 1 ? 's' : ''
+              }`}</p>
             </div>
           ))}
         </div>
-        <div className="mb-8 px-40 md:mb-0 md:w-1/2">
-          <h2 className="mb-8 text-zinc-600 dark:text-zinc-400">Introduce</h2>
-          <h3 className="text-bold mb-12 text-2xl md:text-4xl">
+        <div className="mb-16 md:mb-0 md:w-1/2 md:px-10 lg:px-20 xl:px-28 2xl:px-40">
+          <h2 className="mb-8 text-zinc-600 dark:text-zinc-400">Description</h2>
+          <h3 className="text-bold mb-12 text-4xl xl:text-5xl">
             Hello! I&apos;m Hector Rubí
           </h3>
           <p className="mb-8 text-2xl italic text-zinc-800 dark:text-zinc-200">
             Every great software begin with an even better idea
           </p>
-          <p className="text-zinc-600 dark:text-zinc-400">
+          <p className="text-lg text-zinc-600 dark:text-zinc-400">
             Since beginning my journey nearly 4 years ago, I&apos;ve work for
             different companies and collaborated with talented people to create
             digital products. I&apos;m curious, keep permanently learning and
