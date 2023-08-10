9b13241fc72bc3421130a9da8c43a39b0881e5de
Hector Rubi <hector.rubi.garcia@outlook.com>
2023-08-10 11:07:01 -0600

    Fix information component styles

--- src/components/Information/index.tsx
+++ src/components/Information/index.tsx
@@ -1,23 +1,29 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons'
 import { CONTAINER } from '../constants'
 
 export const Information = () => {
   return (
-    <section>
+    <section className="bg-primary-200 py-36 dark:bg-primary-950">
       <div
-        className={`${CONTAINER} flex flex-col md:flex-row md:justify-between px-5 py-36`}
+        className={`${CONTAINER} flex flex-col md:flex-row md:justify-between`}
       >
-        <div className="mb-10 md:mb-0">
-          <p className="text-2xl lg:text-4xl mb-4">
-            Let&apos;s make something amazing <br className="hidden md:block" />
-            together.
+        <div className="mb-20 max-w-xl md:mb-0 xl:max-w-2xl">
+          <p className="mb-8 text-2xl leading-loose md:text-4xl lg:text-5xl">
+            Let&apos;s make something amazing together.
           </p>
-          <p className="text-2xl lg:text-4xl mb-4">
-            Start by <span className="text-blue-500">saying hi</span>
+          <p className="text-2xl lg:text-4xl">
+            Start by{' '}
+            <span className="text-accent-600 dark:text-accent-400">
+              saying hi
+            </span>
           </p>
         </div>
+
         <div>
-          <h3 className="text-zinc-400 text-xl mb-4">Information</h3>
-          <p className="text-base md:text-xl lg:text-2xl">
+          <h3 className="mb-4 text-xl text-secondary-400">Information</h3>
+          <p className="flex items-center gap-3 text-base md:text-lg lg:text-xl">
+            <FontAwesomeIcon icon={faEnvelope} />
             hector.rubi.garcia@outlook.com
           </p>
         </div>
