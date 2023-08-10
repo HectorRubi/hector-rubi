6d2af2cadf72eafbfaa25c0477e8aea92254b10a
Hector Rubi <hector.rubi.garcia@outlook.com>
2023-08-09 19:07:47 -0600

    Fix colors of navbar finally

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -31,7 +31,7 @@ const links: Link[] = [
 
 export const Navbar = () => {
   return (
-    <nav className="bg-primary-400 dark:bg-primary-900">
+    <nav className="bg-primary-300 dark:bg-primary-800">
       <div className={`${CONTAINER} flex items-center justify-between py-10`}>
         <h3 className="text-2xl">
           <span>Hector </span>
@@ -50,10 +50,10 @@ export const Navbar = () => {
               >
                 <FontAwesomeIcon
                   icon={link.icon}
-                  className={`mr-2 text-lg transition-all group-hover:text-accent-400`}
+                  className={`mr-2 text-lg transition-all group-hover:text-accent-600 dark:group-hover:text-accent-400`}
                 />
                 <span
-                  className={`text-sm text-secondary-600 transition-all group-hover:text-accent-400 dark:text-secondary-400`}
+                  className={`text-sm text-secondary-600 transition-all group-hover:text-accent-600 dark:text-secondary-400 dark:group-hover:text-accent-400`}
                 >
                   {link.name}
                 </span>
@@ -61,7 +61,7 @@ export const Navbar = () => {
             </li>
           ))}
 
-          <li className="group relative flex cursor-pointer items-center justify-center rounded-full bg-primary-300 px-3 py-3 transition-all hover:bg-primary-800 hover:text-accent-400 dark:bg-primary-800 dark:hover:bg-primary-600">
+          <li className="group relative flex cursor-pointer items-center justify-center rounded-full bg-primary-400 px-3 py-3 transition-all hover:bg-primary-800 hover:text-accent-600 dark:bg-primary-900 dark:hover:bg-primary-600 dark:group-hover:text-accent-400">
             <FontAwesomeIcon icon={faEnvelope} />
             <Tooltip text="hector.rubi.garcia@outlook.com" position="right" />
           </li>
