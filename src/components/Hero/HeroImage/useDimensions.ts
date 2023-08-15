a19c7b0f8f42640b1888683be15cea43b4c07823
Hector Rubi <hector.rubi.garcia@outlook.com>
2023-08-15 17:59:40 -0600

    Refactor useDimensions hook to use viewport hook

--- src/components/Hero/HeroImage/useDimensions.ts
+++ src/components/Hero/HeroImage/useDimensions.ts
@@ -1,27 +1,17 @@
 import { RefObject, useEffect, useState } from 'react'
+import { useViewportDimensions } from './../../../hooks/useViewportDimensions'
 
 export const useDimensions = (divRef: RefObject<HTMLDivElement>) => {
   const [dimensions, setDimensions] = useState({ width: 0 })
+  const { isResized } = useViewportDimensions()
 
   useEffect(() => {
-    const getDimensions = () => ({
-      width: divRef.current?.offsetWidth || 0,
-    })
-
-    const handleResize = () => {
-      setDimensions(getDimensions())
-    }
-
     if (divRef.current) {
-      setDimensions(getDimensions())
+      setDimensions({
+        width: divRef.current?.offsetWidth || 0,
+      })
     }
-
-    window.addEventListener('resize', handleResize)
-
-    return () => {
-      window.removeEventListener('resize', handleResize)
-    }
-  }, [divRef])
+  }, [isResized, divRef])
 
   return {
     dimensions,
