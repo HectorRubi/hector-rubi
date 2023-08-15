c5aa9b6a8bb4ea989ea78572a74c9c41f83d2126
Hector Rubi <hector.rubi.garcia@outlook.com>
2023-08-15 17:59:01 -0600

    Create custom hook to handle resize listener

---
+++ src/hooks/useViewportDimensions.ts
@@ -0,0 +1,37 @@
+import { useEffect, useState } from 'react'
+
+export const useViewportDimensions = () => {
+  let defaultValue = { width: 0, height: 0 }
+
+  if (typeof window !== 'undefined') {
+    // Client-side-only code
+    defaultValue = {
+      width: window.innerWidth,
+      height: window.innerHeight,
+    }
+  }
+
+  const [viewport, setViewport] = useState(defaultValue)
+  const [isResized, setIsResized] = useState(false)
+
+  const handleResize = () => {
+    setIsResized(!isResized)
+    setViewport({
+      width: window.innerWidth,
+      height: window.innerHeight,
+    })
+  }
+
+  useEffect(() => {
+    window.addEventListener('resize', handleResize)
+
+    return () => {
+      window.removeEventListener('resize', handleResize)
+    }
+  })
+
+  return {
+    viewport,
+    isResized,
+  }
+}
