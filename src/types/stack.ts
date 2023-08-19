183f442d810f6d2f6779e9bd8f704c8eb24a401c
Hector Rubi <hector.rubi.garcia@outlook.com>
2023-08-18 19:00:48 -0600

    Create IconElement type for my own icons

--- src/types/stack.ts
+++ src/types/stack.ts
@@ -1,7 +1,8 @@
 import { IconDefinition } from '@fortawesome/free-solid-svg-icons'
+import { IconElement } from './IconElement'
 
 export type Stack = {
   icon?: IconDefinition
   text: string
-  element?: (({ className }: { className: string }) => JSX.Element) | undefined
+  element?: IconElement
 }
