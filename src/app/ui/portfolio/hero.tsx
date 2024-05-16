b37cb113997c2b89082b2fc0faa290fa9d57fc88
Hector Rubi <hector.rubi.garcia@outlook.com>
2024-05-16 17:12:27 -0600

    Fix portfolio headline size on sm devices

--- src/app/ui/portfolio/hero.tsx
+++ src/app/ui/portfolio/hero.tsx
@@ -5,8 +5,8 @@ export const Hero = () => {
   return (
     <section className="relative overflow-hidden bg-primary-400 py-20 dark:bg-primary-900">
       <div className={`${CONTAINER} text-center`}>
-        <h1 className="mb-4 text-2xl md:text-4xl">Portfolio</h1>
-        <h2 className="text-sm text-secondary-600 dark:text-secondary-400">
+        <h1 className="mb-4 text-2xl sm:text-3xl md:text-4xl">Portfolio</h1>
+        <h2 className="text-sm text-secondary-600 dark:text-secondary-400 md:text-base">
           Exploring a compilation of my diverse projects
         </h2>
       </div>
