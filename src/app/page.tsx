725b1fafe8f5443fd23b53778bc24ede10c21969
Hector Rubi <hector.rubi.garcia@outlook.com>
2024-05-16 14:16:19 -0600

    Move Latest Works component to ui/home folder inside app folder

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 import { Hero } from '@/app/ui/home/hero'
 import { Description } from '@/components/Home/Description'
-import { LatestWorks } from '@/components/Home/LatestWorks'
+import { LatestWorks } from '@/app/ui/home/latestWorks'
 import { Certifications } from '@/components/Home/Certifications'
 import { TechStack } from '@/components/Home/TechStack'
 
