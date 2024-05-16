cbceb3f15f06a13f150eed2648ed081b7ac400dd
Hector Rubi <hector.rubi.garcia@outlook.com>
2024-05-16 14:22:34 -0600

    Move Certifications component to ui/home folder inside app folder

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 import { Hero } from '@/app/ui/home/hero'
 import { Description } from '@/components/Home/Description'
 import { LatestWorks } from '@/app/ui/home/latestWorks'
-import { Certifications } from '@/components/Home/Certifications'
+import { Certifications } from '@/app/ui/home/certifications'
 import { TechStack } from '@/components/Home/TechStack'
 
 export default function Home() {
