09f4b23be7c9b3b10eec58682951ad36cf04d7f7
Hector Rubi <hector.rubi.garcia@outlook.com>
2024-05-16 14:23:43 -0600

    Move Description component to ui/home folder inside app folder

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 import { Hero } from '@/app/ui/home/hero'
-import { Description } from '@/components/Home/Description'
+import { Description } from '@/app/ui/home/description'
 import { LatestWorks } from '@/app/ui/home/latestWorks'
 import { Certifications } from '@/app/ui/home/certifications'
 import { TechStack } from '@/components/Home/TechStack'
