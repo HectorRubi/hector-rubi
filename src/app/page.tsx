5821405fe13d61e0dd0cfafb4b53353055dc3da3
Hector Rubi <hector.rubi.garcia@outlook.com>
2024-05-16 14:24:52 -0600

    Move Tech Stack component to ui/home folder inside app folder

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import { Hero } from '@/app/ui/home/hero'
 import { Description } from '@/app/ui/home/description'
 import { LatestWorks } from '@/app/ui/home/latestWorks'
 import { Certifications } from '@/app/ui/home/certifications'
-import { TechStack } from '@/components/Home/TechStack'
+import { TechStack } from '@/app/ui/home/techStack'
 
 export default function Home() {
   return (
