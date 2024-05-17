8184fffd21d572938bd7d7d55973de5942fda819
Hector Rubi <hector.rubi.garcia@outlook.com>
2024-05-16 18:11:28 -0600

    Add dynamic rendering to portfolio page

--- src/app/lib/projects/getProjects.ts
+++ src/app/lib/projects/getProjects.ts
@@ -1,3 +1,7 @@
 import prisma from '@/app/lib/primsa'
+import { unstable_noStore as noStore } from 'next/cache'
 
-export const getProjects = async () => await prisma.projects.findMany()
+export const getProjects = async () => {
+  noStore()
+  return await prisma.projects.findMany()
+}
